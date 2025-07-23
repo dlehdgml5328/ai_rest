f408d10c941f1efd85832ce8d3a33316540690de
dlehdgml5328 <dlehdgml5328@naver.com>
2025-07-23 16:23:22 +0900

    [test]

--- proj/settings.py
+++ proj/settings.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 DB_NAME = os.environ.get("DB_NAME")
-load_dotenv(dotenv_path=os.path.join(BASE_DIR, ".env"))
+load_dotenv(dotenv_path=os.path.join(BASE_DIR / ".env", override=True))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
